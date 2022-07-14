816033a307f6a13307435edc8a5f9ffaef1c12dc
Alexander Plaikner <alexander02plaikner.ap@gmail.com>
2022-07-14 17:01:19 +0200

    .

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -6,13 +6,15 @@ import org.springframework.stereotype.Service;
 public class QueryProcessor {
 
     public String process(String query) {
-		query = query.toLowerCase();
+        query = query.toLowerCase();
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("what is your name")) {
-           return "Alexander";
+            return "Alexander";
+        } else if (query.contains("what is 12 plus 12")) {
+            return "24";
         } else { // TODO extend the programm here
             return "";
         }
